362dd28667a0242d7e9c538819c2d3a0d77ba05f
Rusnietis <tskridla@yahoo.com>
2026-03-26 19:42:51 +0200

    Atnaujinta 2026.03.26 Navbar

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -1,100 +1,38 @@
 import { Link, NavLink } from "react-router-dom";
-// import { useContext } from "react";
-import logo from '../assets/logo.svg';
-
-// import { Auth } from '../Contexts/Auth.jsx'
-// import useLogin from "../Hooks/useLogin.jsx";
+import logo from "../assets/logo.svg";
 
 export default function Nav() {
-
-//   const { user } = useContext(Auth);
-//   const { logout } = useLogin();
-
   return (
-    <div className="navbar">
-      <Link to="/" className="logo">
-        <img src={logo} alt="Logo" className="logo" />
-      </Link>
+    <header className="navbar">
+      <div className="navbar__container">
+        <Link to="/" className="navbar__logo">
+          <img src={logo} alt="Logo" />
+        </Link>
 
-      <ul className="navbar-menu">
-        <li>
-          <NavLink to="/apie" end className={({ isActive }) => (isActive ? "active" : "")}>
+        <nav className="navbar__menu">
+          <NavLink to="/apie" end>
             Apie
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/istorijos" className={({ isActive }) => (isActive ? "active" : "")}>
+
+          <NavLink to="/istorijos">
             Istorijos
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/mano-istorijos" className={({ isActive }) => (isActive ? "active" : "")}>
+
+          <NavLink to="/mano-istorijos">
             Mano istorijos
           </NavLink>
-        </li>
-        {/* <li>
-          <NavLink to="/register/list" className={({ isActive }) => (isActive ? "active" : "")}>
-            Vartotojai
+        </nav>
+
+        <div className="navbar__auth">
+          <NavLink to="/login" className="btn btn--secondary">
+            Login
           </NavLink>
-        </li> */}
-        {/* <li>
-          <NavLink to="/admin/dashboard" className={({ isActive }) => (isActive ? "active" : "")}>
-            Admin panelė
+
+          <NavLink to="/register" className="btn btn--primary">
+            Register
           </NavLink>
-        </li> */}
-
-
-      </ul>
-
-      <div>
-            <NavLink
-              to="/login"
-              className="button-18"
-              style={{ cursor: "pointer" }}
-            >
-              Login
-            </NavLink>
-            <span> | </span>
-            <NavLink
-              to="/register"
-              className="button-18"
-              style={{ cursor: "pointer", backgroundColor: "green" }}
-            >
-              Register
-            </NavLink>
-          </div>
-
-      {/* <div className="navbar-right">
-        {user && <span className="user">{user.user}</span>}
-        {user && <span className="sep"> | </span>}
-        {user ? (
-          <i
-            className="button-18 "
-            style={{ cursor: "pointer", backgroundColor: 'red' }}
-            onClick={logout}
-          >
-            Logout
-          </i>
-        ) : (
-          <>
-            <NavLink
-              to="/login"
-              className="button-18"
-              style={{ cursor: "pointer" }}
-            >
-              Login
-            </NavLink>
-            <span> | </span>
-            <NavLink
-              to="/register"
-              className="button-18"
-              style={{ cursor: "pointer", backgroundColor: "green" }}
-            >
-              Register
-            </NavLink>
-          </>
-        )}
-      </div> */}
-    </div>
+        </div>
+      </div>
+    </header>
   );
 }
